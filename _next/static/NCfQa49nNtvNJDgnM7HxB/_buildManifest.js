da1b85eb3c808d579d95cbbba97aa084a74fc856
Ashraf <54310457+afaithraf@users.noreply.github.com>
2020-11-13 22:09:27 +0100

    Deploy gh-pages

---
+++ _next/static/NCfQa49nNtvNJDgnM7HxB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/work":[a]}}("static\u002Fchunks\u002Fff1cf6a6908430db804a5b320b47d2b94782d3ac.6c44f0048d1351987336.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
